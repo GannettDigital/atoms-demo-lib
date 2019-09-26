59ae685a091f3dfc3a9fddfd2d857a8cce7027e6
Shane Tyner <styner@gmti.gannett.com>
2019-09-26 15:21:08 -0400

    Update upnext.js

--- upnext.js
+++ upnext.js
@@ -8,12 +8,14 @@ let article = document.getElementsByTagName('body')[0],
 
 window.onscroll = function() {checkToShowBanner()};
 function checkToShowBanner() {
+    console.log(" UPNEXT SCRIPT running");
     if(!shown && (window.scrollY > (article.offsetHeight * (launchScrollPercentage/100)))) {
         shown = true;
         showBanner();
     }
 }
 function showBanner() {
+    console.log(" UPNEXT SCRIPT running");
     wrapper.classList.add('open');
     setTimeout(function(){
         container.classList.add('open-banner');
